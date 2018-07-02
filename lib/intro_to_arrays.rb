12e14dfc72f67bda102786f596b6248a442e730f
Scott Ungchusri <animoogle504@gmail.com>
2018-07-02 07:42:31 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,27 +1,31 @@
 def instantiate_new_array
-  []
+  return []
 end
 
 def array_with_two_elements
-  [1,2]
+ return [1,2]
 end
 
-def first_element
-  
+def first_element(array)
+ return array[0]
 end
 
-def third_element
-  
+def third_element(array)
+  return array[2]
 end
 
-def last_element
-  
+def last_element(array)
+  return array[array.length-1]
 end
 
-def first_element_with_array_methods
-  
+def first_element_with_array_methods(array)
+  return array.unshift
 end
 
-def last_element_with_array_methods
-  
+def last_element_with_array_methods(array)
+  return array.pop
+end
+
+def length_of_array(array)
+  return array.length_of_array
 end
