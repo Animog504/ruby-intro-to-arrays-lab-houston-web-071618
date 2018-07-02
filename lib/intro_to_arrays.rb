f80669d5ac914c8210a73483d7e80a43aa836755
Scott Ungchusri <animoogle504@gmail.com>
2018-07-02 07:37:14 +0000

    automatically backed up by learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,27 @@
+def instantiate_new_array
+  []
+end
+
+def array_with_two_elements
+  [1,2]
+end
+
+def first_element
+  
+end
+
+def third_element
+  
+end
+
+def last_element
+  
+end
+
+def first_element_with_array_methods
+  
+end
+
+def last_element_with_array_methods
+  
+end
