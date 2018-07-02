0486738ecb4f6ec00b475f4991078b16a150036b
Scott Ungchusri <animoogle504@gmail.com>
2018-07-02 07:44:12 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -19,7 +19,7 @@ def last_element(array)
 end
 
 def first_element_with_array_methods(array)
-  return array.unshift
+  return array.shift
 end
 
 def last_element_with_array_methods(array)
@@ -27,5 +27,5 @@ def last_element_with_array_methods(array)
 end
 
 def length_of_array(array)
-  return array.length_of_array
+  return array.length 
 end
